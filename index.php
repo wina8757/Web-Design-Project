76d9319f9ff0017a76d737af02a057503d3b3648
Jessen Winardinata <jewinardinata@csumb.edu>
2017-08-28 20:03:39 +0000

    adding index.php

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
     <head>
         <title> </title>
         <meta charset-"utf-8"/>
+        
     </head>
     <body>
             
-            <h1> Welcome to Jessen's CST336 website!! </h1>
+            <h1> Welcome to Jessen's CST336 website, May you find something interesting in this site!! </h1>
             
             
     </body>
