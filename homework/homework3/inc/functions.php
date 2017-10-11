452b1f1dd11b1c51ebba43049536e0990d2fb843
Jessen Winardinata <jewinardinata@csumb.edu>
2017-10-11 18:39:27 +0000

    adding homework3

---
+++ homework/homework3/inc/functions.php
@@ -0,0 +1,72 @@
+<?php
+// print_r($_POST);
+?>
+
+<html>
+    <head>
+    </head>
+    <body>
+        
+        <style>
+            @import url("css/styles.css");
+        </style>
+        
+        <h1><center><u>Result: </u></center></h1>
+        <br /><br />
+
+<?php
+    $answer1 = $_POST['question-1-answers'];        
+    $answer2 = $_POST['question-2-answers'];
+    $answer3 = $_POST['question-3-answers'];
+    $answer4 = $_POST['question-4-answers'];
+    $answer5 = $_POST['question-5-answers'];
+        
+    $totalCorrect = 0;
+            
+    if ($answer1 == "B") { $totalCorrect++; }
+    if ($answer2 == "B") { $totalCorrect++; }
+    if ($answer3 == "B") { $totalCorrect++; }
+    if ($answer4 == "A") { $totalCorrect++; }
+    if ($answer5 == "D") { $totalCorrect++; }
+    
+    if($totalCorrect == 5)
+    {
+        echo "<img src= img/wow.gif>";
+    }
+    
+    if(isset($_POST))
+    {
+        $user_fname = $_POST['fname'];
+        $user_lname = $_POST['lname'];
+        $user_id = $_POST['id'];
+        $user_gender = $_POST['male'];
+        $user_gender = $_POST['female'];
+    }
+    
+    
+		if(!isset($_POST['fname'])) 
+		{  //form has not been submitted
+            echo "<h2 style='color:red'>Type your First Name to start the quiz.</h2>";
+        } 
+        else 
+        {   //form has been submitted
+            if (empty($_POST['lname'])  && empty($_POST['id'])  ) 
+            {
+                echo "<h2 style='color:red'> Error! You must enter your Last Name and ID number.</h2>";
+                return;
+                exit;
+            }
+        }
+      
+   echo "<center><font face='Berlin Sans FB' size='8'><strong> First Name: </strong></font></center>" . $user_fname . "<br />";
+    echo "<center><font face='Berlin Sans FB' size='8'><strong> Last Name: </strong></font></center>" . $user_lname . "<br />";
+    echo "<center><font face='Berlin Sans FB' size='8'><strong> User ID : </strong></font></center>" . $user_id . "<br />";
+    echo "<center><font face='Berlin Sans FB' size='8'><strong> Gender: </strong></font></center>" . $user_gender . "<br />";
+    echo"<center><font face='Berlin Sans FB' size='8'>Your Score is <br> $totalCorrect / 5 correct </font></center>";
+    
+   
+?>
+
+
+</body>
+</html>
---
+++ homework/homework3/index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Quiz</title>
+    </head>
+    <body>
+         
+        <style>
+            @import url("css/styles.css");
+        </style>
+        
+        <div id="page-wrap">
+            
+		<h1><u>HTML Quiz</u></h1>
+		
+		<form action="inc/functions.php" method="post" id="quiz">
+		<label for="fname">First Name:</label>
+        <input id="fname" type="text" name="fname" placeholder ="fname" >
+        <br/><br/>
+        <label for="lname">Last Name :</label>
+        <input id="lname" type="text" name="lname" placeholder ="lname" >
+        <br/><br/>
+        <label for="id">ID number  :</label>
+        <input id="id" type="text" name="id" placeholder = "id"  >
+        <br/><br/>
+        <input id="male" type="checkbox" name="genderm" value="male" placeholder ="male">
+        <label for="male">Male</label>
+        <input id="female" type="checkbox" name="genderf" value="female" placeholder ="female">
+        <label for="female">Female</label>
+        <br/><br/>
+        
+		<!--<form action="inc/functions.php" method="post" id="quiz">-->
+            <ol>
+                
+                
+                <li>
+                
+                    <h3>CSS Stands for...</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) Computer Styled Sections </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B) Cascading Style Sheets</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C) Crazy Solid Shapes</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                        <label for="question-1-answers-D">D) None of the above</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>What HTML Form method should be used when submitting confidential information?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
+                        <label for="question-2-answers-A">A) GET</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
+                        <label for="question-2-answers-B">B) POST</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
+                        <label for="question-2-answers-C">C) Password</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-D" value="D" />
+                        <label for="question-2-answers-D">D) Encrypted</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Who is making the Web standards?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
+                        <label for="question-3-answers-A">A) Mozilla</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
+                        <label for="question-3-answers-B">B) The World Wide Web Consortium</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
+                        <label for="question-3-answers-C">C) Google</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-D" value="D" />
+                        <label for="question-3-answers-D">D) Microsoft</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Choose the correct HTML element to define important text</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
+                        <label for="question-4-answers-A">A) Strong</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
+                        <label for="question-4-answers-B">B) important </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
+                        <label for="question-4-answers-C">C) i </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-D" value="D" />
+                        <label for="question-4-answers-D">D) b </label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+                        <label for="question-5-answers-A">A) title </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                        <label for="question-5-answers-B">B) src </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                        <label for="question-5-answers-C">C) longdesc </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
+                        <label for="question-5-answers-D">D) alt </label>
+                    </div>
+                
+                </li>
+            
+            </ol>
+            <input type="submit" value="Submit Quiz" />
+		    <input type="reset" value="Reset" />
+
+		</form>
+	        
+	</div>
+
+    <hr id="footer_hr">
+        <footer>
+            <br />
+            <div id="footer_content">
+                
+            <div id="Img">
+                <img  src="img/buddy.png" alt="BUDDY Logo"/>
+            </div> 
+            
+            <div id="Text">
+            <link href="https://fonts.googleapis.com/css?family=Amita" rel="stylesheet">
+             Designed by Jessen.<br />
+             <strong>&copy;</strong> 2017 Proved by MY BUDDY<br />
+            </div>
+            
+            </div>
+            
+        </footer>
+    </body>
+</html>
